09452080a4ca62fdcba6d8cc09565eb20c244bbe
Fred Rondina <fredrondina96@gmail.com>
2020-05-16 17:22:39 -0600

    created test for import_csv rake task

---
+++ spec/tasks/import_csv_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+Rails.application.load_tasks
+
+describe "import_csv.rake" do
+  it "deletes old customers and loads in new from csv" do
+    customer = Customer.create!(first_name: "Jim", last_name: "Bo")
+    expect(Customer.first.first_name).to eq("Jim")
+    expect(Customer.first.last_name).to eq("Bo")
+    expect(Customer.all.length).to eq(1)
+    Rake::Task["import_csv"].invoke
+    expect(Customer.first.first_name).to eq("Joey")
+    expect(Customer.first.last_name).to eq("Ondricka")
+    expect(Customer.all.length).to eq(1000)
+  end
+end
