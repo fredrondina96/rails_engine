7b3a9f713cea1a136bd5e9e5d00296f02b248af6
Fred Rondina <fredrondina96@gmail.com>
2020-05-18 15:13:06 -0600

    added show method for merchants controller

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Merchants API" do
 
-  it "sends a list of merchants" do
+  it "returns all merchants" do
     merchant1 = Merchant.create!(name: "Jims")
     merchant2 = Merchant.create!(name: "Tims")
     merchant3 = Merchant.create!(name: "Dougs")
@@ -13,4 +13,17 @@ describe "Merchants API" do
     merchants = JSON.parse(response.body)
     expect(merchants["data"].count).to eq(4)
   end
+
+  it "returns the requested merchant" do
+    merchant1 = Merchant.create!(name: "Jims")
+    merchant2 = Merchant.create!(name: "Tims")
+    merchant3 = Merchant.create!(name: "Dougs")
+    merchant4 = Merchant.create!(name: "Bobs")
+
+    get "/api/v1/merchants/#{merchant2.id}"
+    expect(response).to be_successful
+    merchant = JSON.parse(response.body)
+    expect(merchant["data"]["id"]).to eq(merchant2.id.to_s)
+    expect(merchant["data"]["attributes"]["name"]).to eq(merchant2.name)
+ end
 end
