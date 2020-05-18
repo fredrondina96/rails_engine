732b3dc3ff9353866d358e4e8d592a214f1ae8c8
Fred Rondina <fredrondina96@gmail.com>
2020-05-18 15:35:41 -0600

    added merchant update method plus test

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,4 +7,14 @@ class Api::V1::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant)
   end
+
+  def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
+  end
+
+  private
+
+  def merchant_params
+   params.require("merchant").permit("name")
+  end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -26,4 +26,19 @@ describe "Merchants API" do
     expect(merchant["data"]["id"]).to eq(merchant2.id.to_s)
     expect(merchant["data"]["attributes"]["name"]).to eq(merchant2.name)
  end
+
+ it "It can update a merchants attributes" do
+    merchant1 = Merchant.create!(name: "Jims")
+    merchant2 = Merchant.create!(name: "Tims")
+    merchant3 = Merchant.create!(name: "Dougs")
+    merchant4 = Merchant.create!(name: "Bobs")
+    original_name = Merchant.last.name
+
+    new_merchant_params = {name: "The New One"}
+    patch "/api/v1/merchants/#{merchant4.id}", params: {merchant: new_merchant_params}
+    expect(response).to be_successful
+    updated_merchant = Merchant.last
+    expect(updated_merchant.name).to_not eq(original_name)
+    expect(updated_merchant.name).to eq("The New One")
+  end
 end
