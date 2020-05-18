496863910d3f1d29f10112ed591d4dbcca58dbdc
Fred Rondina <fredrondina96@gmail.com>
2020-05-18 16:16:13 -0600

    added update methods to the item controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  private
+
+  def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -24,4 +24,19 @@ describe "items API" do
     expect(item["data"]["id"]).to eq(item3.id.to_s)
     expect(item["data"]["attributes"]["name"]).to eq(item3.name)
   end
+
+  it "can update an item" do
+    merchant1 = Merchant.create!(name: "Jims")
+    item1 = merchant1.items.create!(name: "Example1", description: "D1", unit_price: 31.11)
+    item2 = merchant1.items.create!(name: "Example2", description: "D2", unit_price: 32.22)
+    item3 = merchant1.items.create!(name: "Example3", description: "D3", unit_price: 33.33)
+    original_item = item3
+
+    item_params = {name: "Updated Name", description: "Updated Desc", unit_price: 44.44, merchant_id: merchant1.id}
+    patch "/api/v1/items/#{item3.id}", params: {item: item_params}
+    expect(response).to be_successful
+    updated_item = Item.find(item3.id)
+    expect(updated_item.name).to eq("Updated Name")
+    expect(updated_item.name).to_not eq(original_item.name)
+  end
 end
