ab4364edcf1a89d1704e139da6709b95e3957e7e
Fred Rondina <fredrondina96@gmail.com>
2020-05-18 19:13:42 -0600

    items index now has way for merchants items to be displayed

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: ItemSerializer.new(Item.all)
+    if params[:merchant_id]
+     render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+   else
+     render json: ItemSerializer.new(Item.all)
+   end
   end
 
   def show
