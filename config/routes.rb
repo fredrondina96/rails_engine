a268542b70eb9da423f420f40e63e51430897f87
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 17:54:25 -0600

    re organized routes to make more logical sense

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-   namespace :v1 do
-     resources :merchants, except: [:new, :edit] do
-      resources :items, only: [:index]
-     end
+    namespace :v1 do
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
 
-     resources :items, except: [:new, :edit]
-     get '/items/:item_id/merchant', to: 'merchants#show'
-   end
- end
+      resources :merchants, except: [:new, :edit] do
+        resources :items, only: [:index]
+      end
+
+      resources :items, except: [:new, :edit]
+
+      get '/items/:item_id/merchant', to: 'merchants#show'
+
+    end
+  end
 end
