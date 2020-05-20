d36f7937a5b9c5060885613c755783d1151f15a8
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 18:42:39 -0600

    tests filter by name method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Merchant, type: :model do
     it {should have_many :items}
     it {should have_many :invoices}
   end
+
+  describe "methods" do
+    it "filter_by_name" do
+      merchant1 = Merchant.create!(name: "Jims")
+      merchant2 = Merchant.create!(name: "BoB")
+      merchant3 = Merchant.create!(name: "Tim")
+      merchant4 = Merchant.create!(name: "Timmy")
+      expect(Merchant.filter_by_name('Tim')).to eq([merchant3, merchant4])
+    end
+  end
 end
