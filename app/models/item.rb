ea2cc6c36f741615b62f7022b8129c96512299be
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 18:13:15 -0600

    added filter methods to item model and tests, can filter by all but date

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,15 @@ class Item <ApplicationRecord
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
 
+  def self.filter_by_name(name)
+    @items = self.where(Item.arel_table[:name].lower.matches("%#{name.downcase}%"))
+  end
 
+  def self.filter_by_description(description)
+    @items = self.where(Item.arel_table[:description].lower.matches("%#{description.downcase}%"))
+  end
+
+  def self.filter_by_unit_price(unit_price)
+    @items = self.where(unit_price: unit_price)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,31 @@ describe Item, type: :model do
     it { should have_many :invoice_items}
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe "methods" do
+    it "can filter items by name" do
+      merchant1 = Merchant.create!(name: "Jims")
+      item1 = merchant1.items.create!(name: "chair", description: "you can sit on it", unit_price: 31.11)
+      item2 = merchant1.items.create!(name: "Example2", description: "D2", unit_price: 32.22)
+      item3 = merchant1.items.create!(name: "aeiou", description: "D3", unit_price: 33.33)
+      expect(Item.filter_by_name('chair')).to eq([item1])
+    end
+
+    it "can filter items by description" do
+      merchant1 = Merchant.create!(name: "Jims")
+      item1 = merchant1.items.create!(name: "chair", description: "you can sit on it", unit_price: 31.11)
+      item2 = merchant1.items.create!(name: "Example2", description: "D2", unit_price: 32.22)
+      item3 = merchant1.items.create!(name: "aeiou", description: "D3", unit_price: 33.33)
+      expect(Item.filter_by_description('D')).to eq([item2, item3])
+    end
+
+    it "can filter items by unit_price" do
+      merchant1 = Merchant.create!(name: "Jims")
+      item1 = merchant1.items.create!(name: "chair", description: "you can sit on it", unit_price: 31.11)
+      item2 = merchant1.items.create!(name: "Example2", description: "D2", unit_price: 32.22)
+      item3 = merchant1.items.create!(name: "aeiou", description: "D3", unit_price: 33.33)
+      expect(Item.filter_by_unit_price(33.33)).to eq([item3])
+    end
+
+  end
 end
