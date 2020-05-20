1b315288949a9e9405cd33119eb1646d9345365c
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 18:22:56 -0600

    added index method for returning all items that match a query

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def show
-      @items = filter
-      render json: ItemSerializer.new(@items.first)
-    end
-
-    private
-
-    def filter
-      @items = Item.all
-      @items = @items.filter_by_name(params[:name]) if params[:name].present?
-      @items = @items.filter_by_description(params[:description]) if params[:description].present?
-      @items = @items.filter_by_unit_price(params[:unit_price]) if params[:unit_price].present?
-      @items
-    end
+    @items = filter
+    render json: ItemSerializer.new(@items.first)
   end
+
+  def index
+    @items = filter
+    render json: ItemSerializer.new(@items)
+  end
+
+  private
+
+  def filter
+    @items = Item.all
+    @items = @items.filter_by_name(params[:name]) if params[:name].present?
+    @items = @items.filter_by_description(params[:description]) if params[:description].present?
+    @items = @items.filter_by_unit_price(params[:unit_price]) if params[:unit_price].present?
+    @items
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :items do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       resources :merchants, except: [:new, :edit] do
