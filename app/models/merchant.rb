36c4c14e48c3b34b516bda7a652f8e6615dfc66f
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 18:42:28 -0600

    added filter by name method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+
+  def self.filter_by_name(name)
+    @merchants = self.where(Merchant.arel_table[:name].lower.matches("%#{name.downcase}%"))
+  end
 end
